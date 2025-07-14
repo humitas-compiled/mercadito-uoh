ccd9dc18b0170879ecbca0310d89fb77cdf47471
Catalina <catalina.lyllo@gmail.com>
2025-07-14 03:28:55 -0400

    cambios en compras

--- compras/urls.py
+++ compras/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# Lista urls para funcionalida seccion compras
+# Lista urls para funcionalidad seccion compras
 urlpatterns = [
     path('', views.seccion_compras, name="compras"),
     path('ver-producto/<int:id>/', views.ver_producto, name='ver_producto'),
