1657912151ed4875ed37fb5e9958b4cb60ce9005
enzo <enzopaolo25@gmail.com>
2025-07-14 03:25:30 -0400

    cambios

--- perfil_user/admin.py
+++ perfil_user/admin.py
@@ -3,30 +3,30 @@ from .models import Perfil
 from django.utils import timezone
 from datetime import timedelta
 
-@admin.register(Perfil)  #debe ser un admin registrado
+@admin.register(Perfil)  #debe ser un admin 
 class PerfilAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email', 'telefono', 'campus', 'esta_suspendido')
-    actions = ['suspender_3_dias', 'suspender_7_dias', 'quitar_suspension']
+    list_display = ('user', 'email', 'telefono', 'campus', 'esta_suspendido') #lista a mostrar
+    actions = ['suspender_3_dias', 'suspender_7_dias', 'quitar_suspension'] #acciones
 
-    @admin.action(description='Suspender por 3 días')
+    @admin.action(description='Suspender por 3 días') #cuando se escoge la suspensión por tres dias
     def suspender_3_dias(self, request, queryset):
         for perfil in queryset:
             perfil.suspendido_hasta = timezone.now() + timedelta(days=3)
             perfil.suspension = True
             perfil.save()
 
-    @admin.action(description='Suspender por 7 días')
+    @admin.action(description='Suspender por 7 días') #cuando se escoge la suspensión por siete dias
     def suspender_7_dias(self, request, queryset):
         for perfil in queryset:
             perfil.suspendido_hasta = timezone.now() + timedelta(days=7)
             perfil.suspension = True
             perfil.save()
 
-    @admin.action(description='Quitar suspensión')
+    @admin.action(description='Quitar suspensión') #se quita la suspensión
     def quitar_suspension(self, request, queryset):
-        queryset.update(suspendido_hasta=None)
+        queryset.update(suspendido_hasta=None, suspension = False)
 
-    def esta_suspendido(self, obj):
+    def esta_suspendido(self, obj): #si está suspendido
         return obj.esta_suspendido()
     esta_suspendido.boolean = True
     esta_suspendido.short_description = '¿Suspendido?'
--- perfil_user/models.py
+++ perfil_user/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+#creamos el modelo perfil con esos campos
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=20)
     email = models.EmailField()
     campus = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='perfiles/', blank=True, null=True)
+    imagen = models.ImageField(upload_to='perfiles/', blank=True, null=True) #en la bd esto es un string (pero acá será tipo imagen)
     suspension = models.BooleanField(default=False)
     suspendido_hasta = models.DateTimeField(blank=True, null=True)
 
