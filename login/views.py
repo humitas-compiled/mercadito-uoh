027a6d19ed71cf2abe825a3005e90feb644523af
Catalina <catalina.lyllo@gmail.com>
2025-07-14 03:48:58 -0400

    cambios en compras

--- login/views.py
+++ login/views.py
@@ -6,33 +6,36 @@ from django.contrib import messages
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.decorators import login_required
 from perfil_user.models import Perfil
-# Create your views here.
 
+# Vista para manejar el inicio de sesion
 def login_view(request):
     if request.method == 'POST':
+        # Obtiene el correo y la contraseña desde el formulario
         correo = request.POST['correo']
         password = request.POST['password']
 
         try:
-            usuario = User.objects.get(email=correo)
+            usuario = User.objects.get(email=correo) #Busca al usuario por su correo 
 
-            if check_password(password, usuario.password):
-                login(request, usuario)
-                return redirect('compras')  #funcion de vista dsp del login
+            if check_password(password, usuario.password): #Verifica la contraseña
+                login(request, usuario) #Inicia sesion y redirige a pagina de compras   
+                return redirect('compras')  #Vista que se muestra despues del login
             else:
-                messages.error(request, "Contraseña incorrecta.")
+                messages.error(request, "Contraseña incorrecta.") #Mensaje mostrado en caso de contraseña incorrecta
         except User.DoesNotExist:
-            messages.error(request, "Correo no registrado.")
+            messages.error(request, "Correo no registrado.") #Correo no encontrado en la base de datos
 
     return render(request, 'login/login.html')
 
+# Vista principal despues de iniciar sesion
 @login_required
 def inicio(request):
-    if Perfil.suspension:
+    if Perfil.suspension: #Verifica si el usuario esta suspendido
         raise Http404("Estás suspendido!")
-    else:
+    else: #Si no esta suspendido, muestra la pagina principal
         return render(request, 'compras/index.html')
 
+# Vista para cerrar sesion
 def logout_view(request):
     logout(request)
-    return redirect('login')  #redirige a tu página de login
+    return redirect('login')  #Termina sesion y redirige a la página de login
